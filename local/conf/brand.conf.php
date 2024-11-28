f7bca1ac2752db3725ee4e0df0ad8645a807cb61
Erwin Palma <30660343+erwinpalma@users.noreply.github.com>
2024-11-28 13:13:27 +0100

    zabbix 7

--- local/conf/brand.conf.php
+++ local/conf/brand.conf.php
@@ -1,8 +1,8 @@
 <?php
 return [
-	'BRAND_LOGO' => './images/costum_logo.png',
-	'BRAND_LOGO_SIDEBAR' => './images/logo_sidebar.png',
-	'BRAND_LOGO_SIDEBAR_COMPACT' => './images/logo_compact.png',
+	'BRAND_LOGO' => './rebranding/logo.svg',
+	'BRAND_LOGO_SIDEBAR' => './rebranding/logo_sidebar.png',
+	'BRAND_LOGO_SIDEBAR_COMPACT' => './rebranding/logo_compact.png',
 	'BRAND_FOOTER' => 'Jesus , Palma & Romero S.A. de C.V.',
 	'BRAND_HELP_URL' => 'https://soporte.jpyrsa.net'
 ];
